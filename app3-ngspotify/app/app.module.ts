987238038367b2fa8fb61668c66a5e55c5cecb53
Serhii Panov <spanov@integrity.com.ua>
2016-09-29 16:20:06 +0300

    app3 lec18 : add routing
    https://www.udemy.com/learn-angular-2-development-by-building-10-apps/learn/v4/t/lecture/5311454

--- app3-ngspotify/app/app.module.ts
+++ app3-ngspotify/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { routing } from './app.routing';
 
 import { AppComponent }  from './app.component';
 import {NavbarComponent} from './components/navbar/navbar.component';
+import {SearchComponent} from './components/search/search.component';
+import {AboutComponent} from './components/about/about.component';
 
 @NgModule({
-  imports: [ BrowserModule ],
+  imports: [ BrowserModule, routing ],
   declarations: [
     AppComponent,
     NavbarComponent,
+    SearchComponent,
+    AboutComponent,
   ],
   bootstrap: [ AppComponent ]
 })
