12ed0180d07a1ec348f3f688bcb0d512a20ea4a4
Sergey Panov <gostroy@gmail.com>
2016-10-01 00:57:10 +0300

    app5 lec35 : add Firebase code
    https://www.udemy.com/learn-angular-2-development-by-building-10-apps/learn/v4/t/lecture/5311508

--- app5-firebasebuscont/src/app/app.module.ts
+++ app5-firebasebuscont/src/app/app.module.ts
@@ -2,17 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
+
 
 import { AppComponent } from './app.component';
 
+// Must export the config
+export const firebaseConfig = {
+  apiKey: "AIzaSyDIQQAeD94bVUXdcahROORsI3I_6MqAPfk",
+  authDomain: "fir-buscont.firebaseapp.com",
+  databaseURL: "https://fir-buscont.firebaseio.com",
+  storageBucket: "",
+  messagingSenderId: "156047303901"
+};
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
   ],
   providers: [],
   bootstrap: [AppComponent]
