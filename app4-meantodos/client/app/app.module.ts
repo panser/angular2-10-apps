e7c76369b7b9723aef19c5849928a4c6890dcb4a
Sergey <gostroy@gmail.com>
2016-09-30 15:05:48 +0300

    app4 lec29 : add TodosComponent
    > npm start
     > npm run tsc:w
    
     https://www.udemy.com/learn-angular-2-development-by-building-10-apps/learn/v4/t/lecture/5311488

--- app4-meantodos/client/app/app.module.ts
+++ app4-meantodos/client/app/app.module.ts
@@ -1,6 +1,8 @@
 import {NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
+
 import {AppComponent} from "./app.component";
+import {TodosComponent} from "./components/todos.component";
 
 @NgModule({
   imports: [
@@ -8,6 +10,7 @@ import {AppComponent} from "./app.component";
   ],
   declarations: [
     AppComponent,
+    TodosComponent,
   ],
   bootstrap: [ AppComponent ]
 })
