ec155e2e2a87b75fc0591f801ec6f0193d9743c1
Sergey Panov <gostroy@gmail.com>
2016-10-12 17:53:56 +0300

    app5 fix temporary comment 3eeaafb : now firebase typings work without problems

--- app5-firebasebuscont/src/typings.d.ts
+++ app5-firebasebuscont/src/typings.d.ts
@@ -2,5 +2,6 @@
 // https://github.com/typings/typings
 // https://www.typescriptlang.org/docs/handbook/writing-declaration-files.html
 
-declare var System: any;
 ///<reference path="../node_modules/firebase/firebase.d.ts"/>
+
+declare var System: any;
